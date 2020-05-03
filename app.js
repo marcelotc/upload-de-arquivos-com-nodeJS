76ff69ab6f6c5e54355028aac55f31c03937da21
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-05-03 01:07:04 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
@@ -9,28 +10,14 @@ const rotaUsuarios = require('./routes/usuarios');
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
-app.use(bodyParser.json()); // json de entrada no body
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 app.use('/usuarios', rotaUsuarios);
 
-// Quando não encontra rota, entra aqui:
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado');
     erro.status = 404;
@@ -43,7 +30,8 @@ app.use((error, req, res, next) => {
         erro: {
             mensagem: error.message
         }
-    });
+    })
 });
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http');
 const app = require('./app');
-const server = http.createServer(app);
-server.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port);
