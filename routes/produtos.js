fc79c288e8b7176d7b8921dee1679edf57d72e48
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-05-02 22:44:38 -0300

    protegendo rotas com JWT

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = new Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
+const login = require('../middleware/login');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -63,14 +64,14 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', upload.single('produto_imagem'), (req, res, next) => {
+router.post('/', login.obrigatorio, upload.single('produto_imagem'), (req, res, next) => {
     /*
     const produto = {
         nome: req.body.nome,
         preco: req.body.preco
     }
     */
-    console.log(req)
+    console.log(req.usuario)
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
@@ -134,7 +135,7 @@ router.get('/:id_produto', (req, res, next) => {
     });
 });
 
-router.patch('/', (req, res, next) => {
+router.patch('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
@@ -164,7 +165,7 @@ router.patch('/', (req, res, next) => {
     })
 });
 
-router.delete('/', (req, res, next) => {
+router.delete('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
